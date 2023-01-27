5457d9bc896e487538c451960af688e7e47b4a01
miguelMartinezL <miguelma.lucero@gmail.com>
2023-01-27 15:12:01 -0800

    Basic structure of the framework

---
+++ src/FrameworkApplication.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class FrameworkApplication {
+    public void run(){
+
+        try{
+
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
+}
---
+++ src/handler/HttpHandler.java
@@ -0,0 +1,12 @@
+package handler;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class HttpHandler {
+    public void handle(HttpExchange httpExchange) throws IOException
+    {
+
+    }
+}
